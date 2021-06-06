279cb4e64490ed1a24100e63e81ac66dbdf0e1d4
celebiali <ali1999_@hotmail.com>
2021-06-06 13:00:34 +0300

    hakkımda

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,23 @@
 function HomePage() {
-  return <div className="text-purple-500 bg-blue-100">Welcome to Next.js!</div>
+  return (
+    <div className="site-container">
+      <div className="space-y-4">
+        <h1 className="text-2xl font-bold">
+          Ben Ali ÇELEBİ. Bilgisayar Mühendisiyim. Frontend alanında kendimi
+          geliştiriyorum.
+        </h1>
+      </div>
+      <img
+        style={{
+          width: 350,
+          height: 350,
+          borderRadius: 350
+        }}
+        className="mx-auto mt-10"
+        src="A.4cb642fa.jpg"
+      />
+    </div>
+  )
 }
 
 export default HomePage
