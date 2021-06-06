43f8b1ee0517b9c327889d96c453fae560cc0ca3
celebiali <ali1999_@hotmail.com>
2021-06-06 19:15:05 +0300

    4-adım

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,21 @@
 import '../styles/global.css'
 import Header from '../components/header'
+import { Auth0Provider } from "@auth0/auth0-react";
 
 function MyApp({ Component, pageProps }) {
   return (
+    <Auth0Provider
+      domain="dev-3l0nmv5r.us.auth0.com"
+      clientId="NXnkjixQsTlBURoLj5NsvrfsOKDeQQu4"
+      redirectUri={process.env.NEXT_PUBLIC_URL}
+    >
     <div className="antialiased text-gray-700">
       <Header />
       <main className="mt-6 mb-20">
         <Component {...pageProps} />
       </main>
     </div>
+    </Auth0Provider>
   )
 }
 
--- pages/blog/[...slug].js
+++ pages/blog/[...slug].js
@@ -1,8 +1,10 @@
 import { getMdxNode, getMdxPaths } from 'next-mdx/server'
 import { useHydrate } from 'next-mdx/client'
 import { mdxComponents } from '../../components/mdx-components'
+import { useAuth0 } from '@auth0/auth0-react'
 
 export default function PostPage({ post }) {
+  const {loginWithRedirect,logout,isAuthenticated,user} = useAuth0();
 
   const content = useHydrate(post, {
     components: mdxComponents
@@ -16,7 +18,21 @@ export default function PostPage({ post }) {
         <hr />
         <div>{content}</div>
       </article>
+    <form className="mt-10">
 
+      <textarea rows="3" className="border border-gray-300 rounded w-full block px-2 py-1"/>
+      <div className="mt-4">
+        {isAuthenticated ?(
+          <div className="flex items-center space-x-2">
+            <button className="bg-blue-600 px-2 py-1 text-white rounded">Send</button>
+            <img src={user.picture} width={30} className="rounded-full"/>
+            <span>{user.name}</span>
+            <button typeof="button" onClick={() => logout({returnTo: process.env.NEXT_PUBLIC_URL + '/blog'})}>x</button>
+          </div>):(<div>
+          <button className="bg-blue-600 px-2 py-1 text-white rounded" typeof="button" onClick={() => loginWithRedirect()}>Login</button>
+        </div>)}
+      </div>
+    </form>
    </div>
   )
 }
